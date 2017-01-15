da893a3c328398f83b3ca15a41c9ad8794790388
Dave Cheli <dcheli@live.com>
2017-01-14 18:07:52 -0600

    Send IAPReceipt to itunes and return result (works).

--- src/main/java/net/services/api/specasap/resources/IAPReceiptResource.java
+++ src/main/java/net/services/api/specasap/resources/IAPReceiptResource.java
@@ -3,12 +3,13 @@ package net.services.api.specasap.resources;
 import java.io.IOException;
 
 import javax.inject.Singleton;
+import javax.json.JsonObject;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -28,15 +29,19 @@ public class IAPReceiptResource {
 	
 	@POST
 	@Path("/verifyapplereceipt")
-	@Produces(MediaType.APPLICATION_JSON)
-	public IAPReceipt verifyReceipt(@Context UriInfo uriInfo,
-			@QueryParam("r") String receipt,
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String verifyReceipt(@Context UriInfo uriInfo,
+			String receipt,
 			@Context HttpServletRequest request)throws IOException {
-		System.out.println("Hello receipt");
+		//you need to get what is sent in the body of the request
+		System.out.println("/verifyapplereceipt is called with ");
+		//logger.error("/verifyapplereceipt is called with " );
 		IAPReceiptService iapReceiptService = new IAPReceiptService(request.getServletContext());
-		iapReceiptService.verifyAppleReceipt(receipt);
+		String jsonReceipt = iapReceiptService.verifyAppleReceipt(receipt);
+		System.out.println("received " + jsonReceipt);
 	
-		return null;
+		return jsonReceipt;
 	}
 
 }
--- src/main/java/net/services/api/specasap/services/IAPReceiptService.java
+++ src/main/java/net/services/api/specasap/services/IAPReceiptService.java
@@ -2,17 +2,27 @@ package net.services.api.specasap.services;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.Base64;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonString;
+import javax.json.JsonValue;
 import javax.servlet.ServletContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import org.apache.log4j.Logger;
+
+
 import net.services.api.specasap.model.IAPReceipt;
 
 
@@ -29,33 +39,27 @@ public class IAPReceiptService {
 
 	}
 	
-	public IAPReceipt verifyAppleReceipt(final String receipt) {
+	public String verifyAppleReceipt(final String receipt) throws UnsupportedEncodingException, IOException {
 		// Make call to iTunes
-		String iTunesURL = "https:sandbox.itunes.apple.com/verifyReceipt";
-		//final Base64Encoder encoder = new Base64Encoder();
-	    String receiptData = "";
-		try {
-			receiptData = Base64.getEncoder().encodeToString(receipt.getBytes("utf-8"));
-		} catch (UnsupportedEncodingException e) {
-			logger.error("verifyAppleReceipt: " + e);
-		}
+		String iTunesURL = "https://sandbox.itunes.apple.com/verifyReceipt";
+		String sharedSecret = "cc22d11bdd4348a4aa9d89c5880e0073";
+	    //final String productionUriStr = "https://buy.itunes.apple.com/verifyReceipt";
+		//final String receiptData = encoder.encode(receipt.getBytes());
 
-	    final String jsonData = "{\"receipt-data\" : \"" + receiptData + "\"}";
+		String jsonData = "{\"receipt-data\" : \"" + receipt + "\", \"password\": \"" + sharedSecret + "\"}";
 
-	    System.out.println(receipt);
-	    System.out.println(jsonData);
-	    
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(iTunesURL);
-		Builder builder = target.request(MediaType.APPLICATION_JSON);
+	    logger.error("trying to verifyAppleReceipt");
+		final Client client = ClientBuilder.newClient();
+		final WebTarget target = client.target(iTunesURL);
+		final Builder request = target.request(MediaType.APPLICATION_FORM_URLENCODED);
+		System.out.println("Callin itunes");
+		Response response = request.post(Entity.json(jsonData));
 		
-//		Response response = builder.get();
-		//System.out.println(response);
-		
-		return null;
+		//System.out.println("Made call to itunes... now what " + response.readEntity(String.class));
+		 logger.error("Made call to itunes... now what " + response.getStatus());
+		 logger.error("Response is " + response);
+		  return response.readEntity(String.class);
 	}
-
-	
 	
 	
 }
